7211509bc608f13aaaa3fc93db6765af3bcf26ce
tatacheba <tanya.cheba12@gmail.com>
2022-02-11 20:31:40 +0300

    add homework Lesson02

--- src/test/java/junit/Les02TestForm.java
+++ src/test/java/junit/Les02TestForm.java
@@ -1,30 +1,52 @@
 package junit;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.*;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Les02TestForm {
 
     @BeforeAll
-    void openBrowser() {
-        Configuration.baseUrl = "https://demoqa.com";
-    }
-
-    @AfterEach
-    void closeBrowser() {
-        Selenide.closeWindow();
+    static void openBrowser() {
+        baseUrl = "https://demoqa.com";
+        browserSize = "1920x1080";
     }
 
     @Test
-    void fillTextForm() {
-        open("automation-practice-form");
-        $("").shouldHave("");
+    void fillTextForm() throws InterruptedException {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper > h5").shouldHave(text("Student Registration Form"));
+        $("#firstName").setValue("Steve");
+        $("#lastName").setValue("Rogers");
+        $("#userEmail").setValue("captainamerica@marvel.com");
+        $(byText("Male")).click();
+        $("#userNumber").setValue("8885115480");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOptionByValue("1918");
+        $(".react-datepicker__month-select").selectOptionByValue("6");
+        $(".react-datepicker__day--004").click();
+        $("#subjectsInput").setValue("Art").pressEnter();
+        $(byText("Sports")).click();
+        $("#userEmail").setValue("captainamerica@marvel.com");
+        $("#uploadPicture").uploadFile(new File("src/test/resources/capitanamerica.jpg"));
+        $("#currentAddress").setValue("Earth-616, New York City");
+        $("#state").click();
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#city").click();
+        $("#react-select-4-input").setValue("Noi").pressEnter();
+        $(byText("Submit")).click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $("table").shouldHave(text("Steve Rogers"), text("captainamerica@marvel.com"), text("Male"), text("8885115480"), text("04 July,1918"), text("Arts"),
+                text("Sports"), text("capitanamerica.jpg"), text("Earth-616, New York City"), text("NCR Noida"));
+        $("#closeLargeModal").shouldHave(text("Close"));
 
-        
+
     }
 }
 
